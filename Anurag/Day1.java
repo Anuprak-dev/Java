4154bf8023a87d284f83d7cb12039cecc982ade2
Parul Saraogi <parulsaraogi@Paruls-MacBook-Air.local>
2025-11-24 15:45:11 +0530

    day2

---
+++ Anurag/Day1.java
@@ -0,0 +1,7 @@
+package Anurag;
+
+public class Day1 {
+    public static void main(String[] args) {
+        System.err.println("Anurag. jjj");
+    }
+}
---
+++ Anurag/Day2.java
@@ -0,0 +1,9 @@
+package Anurag;
+
+public class Day2 {
+    public static void main(String[] args) {
+
+        System.out.println("pratiksha");
+        
+    }
+}
